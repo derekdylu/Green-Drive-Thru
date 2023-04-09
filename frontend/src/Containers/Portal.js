c26c686e33cc434ae51034ce07f91a8cdb0716a9
Derek Lu <derekdylu@gmail.com>
2023-04-10 01:34:19 +0800

    v1

---
+++ frontend/src/Containers/Portal.js
@@ -0,0 +1,229 @@
+import React, { useState } from 'react'
+
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+import header from '../Images/header.png'
+import { timberData } from '../Utils/TimberData';
+
+const TRANSFORM_CONST = 8.8
+
+const Portal = () => {
+  const [currentTimber, setCurrentTimber] = useState(timberData[0])
+  const [currentImport, setCurrentImport] = useState(0)
+  const [timbers, setTimnbers] = useState([])
+
+  const handleChangeTimber = (event) => {
+    setCurrentTimber(event.target.value)
+  }
+
+  const handleChangeImport = (event) => {
+    setCurrentImport(event.target.value)
+  }
+
+  const handleAddTimber = () => {
+    if (timbers.find((item) => item.speciesName === currentTimber.speciesName)) {
+      alert("木材已存在")
+      return
+    }
+    const newTimeber = {
+      "name": currentTimber.name,
+      "speciesName": currentTimber.speciesName,
+      "cf": currentTimber.cf,
+      "import": currentImport,
+      "reduction": Math.round((currentImport * TRANSFORM_CONST * currentTimber.cf) * 1000) / 1000,
+    }
+    setTimnbers([...timbers, newTimeber])
+    setCurrentImport(0)
+    setCurrentTimber(timberData[0])
+  }
+
+  const handleDeleteTimber = (index) => {
+    const newTimbers = timbers.filter((item, i) => i !== index)
+    setTimnbers(newTimbers)
+  }
+
+  return (
+    <div style={{ background: "#e3e3e3", paddingBottom: "500px"}}>
+      <img src={header} alt="header" style={{ width: "100vw", height: "200px", objectFit: "cover"}}/>
+      <Typography variant="h6" sx={{ px: 7 }}>
+        {(() => {
+          let car = [];
+          for (let i = 1; i <= Math.round(timbers.reduce((acc, cur) => acc + cur.reduction, 0) / 4.6); i++) {
+            car.push(<>🚗</>);
+          }
+          return car;
+        })()}
+      </Typography>
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-start"
+        sx={{
+          p: 3,
+        }}
+      >
+        <>
+          {
+            timbers.length > 0 &&
+            (
+              <>
+                <Grid
+                  container
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  sx={{
+                    p: 3,
+                    m: 2,
+                  }}
+                  style={{ 
+                    background: "#ffffff",
+                    width: "340px",
+                    borderRadius: "16px",
+                  }}
+                >
+                  <Typography variant="h4">
+                    ♻️
+                  </Typography>
+                  <Button variant="contained" color="primary" disabled sx={{ mb: 4}}>
+                    建立完整的報告書
+                  </Button>
+                  <Typography variant="h4">
+                    🪵
+                  </Typography>
+                  <Button variant="contained" color="primary" disabled>
+                    什麼是負碳產品？
+                  </Button>
+                </Grid>
+                <Grid
+                  container
+                  direction="column"
+                  sx={{
+                    p: 3,
+                    m: 2,
+                  }}
+                  style={{ 
+                    background: "#ffffff",
+                    width: "340px",
+                    borderRadius: "16px",
+                  }}
+                >
+                  <Typography variant="h6">
+                    透過使用國產木材
+                  </Typography>
+                  <Typography variant="h6">
+                    一年為減少的CO2e約為
+                  </Typography>
+                  <Typography variant="h3">
+                    {
+                      Math.round(timbers.reduce((acc, cur) => acc + cur.reduction, 0))
+                    } 公噸
+                  </Typography>
+                  <Typography variant="h6" sx={{ mt: 1}}>
+                    約等於
+                  </Typography>
+                  <Typography variant="h3">
+                    {
+                      Math.round(timbers.reduce((acc, cur) => acc + cur.reduction, 0) / 4.6)
+                    }
+                  </Typography>
+                  <Typography variant="h6">
+                    輛汽車一年的CO2e
+                  </Typography>
+                </Grid>
+              </>
+            )
+          }
+        </>
+        <>
+          {
+            timbers.map((item, index) => {
+              return(
+                <Grid
+                  container
+                  direction="column"
+                  sx={{
+                    p: 3,
+                    m: 2,
+                  }}
+                  style={{ 
+                    background: "#ffffff",
+                    width: "340px",
+                    borderRadius: "16px",
+                  }}
+                  key={index}
+                >
+                  <Typography variant="subtitle2" sx={{ mt: 0.5 }}>
+                    木材種類
+                  </Typography>
+                  <Typography variant="body1">
+                    {item.name} {item.speciesName}
+                  </Typography>
+                  <Typography variant="subtitle2" sx={{ mt: 2 }}>
+                    年進貨材積
+                  </Typography>
+                  <Typography variant="body1">
+                    {item.import * 1000} BMF
+                  </Typography>
+                  <Typography variant="subtitle2" sx={{ mt: 2 }}>
+                    固定的二氧化碳
+                  </Typography>
+                  <Typography variant="body1">
+                    {item.reduction} 公噸
+                  </Typography>
+                  <Button variant="outlined" color="error" onClick={() => handleDeleteTimber(index)} sx={{ mt: 5.025 }}>移除</Button>
+                </Grid>
+              )
+            })
+          }
+        </>
+        <Grid
+          container
+          direction="column"
+          sx={{
+            p: 3,
+            m: 2,
+          }}
+          style={{ 
+            background: "#ffffff",
+            width: "340px",
+            borderRadius: "16px",
+          }}
+        >
+          <Typography variant="h6">
+            新增木材
+          </Typography>
+          <FormControl fullWidth sx={{ mt: 2 }}>
+            <InputLabel id="demo-simple-select-label">木材種類</InputLabel>
+            <Select
+              value={currentTimber}
+              label="treeType"
+              onChange={handleChangeTimber}
+            >
+              {
+                timberData.map((item, index) => {
+                  return <MenuItem value={item} key={index}>{item.name}</MenuItem>
+                })
+              }
+            </Select>
+          </FormControl>
+          <Typography variant="caption" sx={{ mt: 0.2 }}>
+            {currentTimber ? currentTimber.speciesName : ""}
+          </Typography>
+          <TextField label="年進貨量 (材積；千 BMF)" variant="outlined" value={currentImport} onChange={handleChangeImport} sx={{ mt: 2 }} />
+          <Button variant="contained" color="primary" onClick={() => handleAddTimber()} disabled={!(currentImport > 0)} sx={{ mt: 2 }}>新增</Button>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default Portal
---
+++ frontend/src/Utils/TimberData.js
@@ -0,0 +1,132 @@
+export const timberData = [
+  { 
+    "name": "台灣肖楠",
+    "speciesName": "Calocedrus formosana",
+    "cf": 0.262,
+  },
+  { 
+    "name": "紅檜",
+    "SpeciesName": "Chamaecyparis formosensis",
+    "cf": 0.204,
+  },
+  { 
+    "name": "台灣扁柏",
+    "speciesName": "Cryptomeria obtusa",
+    "cf": 0.203,
+  },
+  { 
+    "name": "柳杉",
+    "speciesName": "Cryptomeria japonica",
+    "cf": 0.177,
+  },
+  {
+    "name": "福州杉",
+    "speciesName": "Cunninghamia lanceolata",
+    "cf": 0.150,
+  },
+  {
+    "name": "台灣雲杉",
+    "speciesName": "Picea morrisonicola",
+    "cf": 0.221,
+  },
+  {
+    "name": "台灣二葉松",
+    "speciesName": "Pinus taiwanensis",
+    "cf": 0.259,
+  },
+  {
+    "name": "台灣雲杉",
+    "speciesName": "Picea morrisonicola",
+    "cf": 0.221,
+  },
+  {
+    "name": "台灣杉",
+    "speciesName": "Tawania cryptomerioides",
+    "cf": 0.155,
+  },
+  {
+    "name": "台灣鐵杉",
+    "speciesName": "Tsuga chinensis",
+    "cf": 0.205,
+  },
+  {
+    "name": "相思樹",
+    "speciesName": "Picea morrisonicola",
+    "cf": 0.221,
+  },
+  {
+    "name": "台灣雲杉",
+    "speciesName": "Acacia confusa",
+    "cf": 0.363,
+  },
+  {
+    "name": "台灣赤楊",
+    "speciesName": "Alnus japonica",
+    "cf": 0.217,
+  },
+  {
+    "name": "茄冬",
+    "speciesName": "Bischofia javanica",
+    "cf": 0.304,
+  },
+  {
+    "name": "木麻黃",
+    "speciesName": "Casuarina equisetifolia",
+    "cf": 0.312,
+  },
+  {
+    "name": "樟木",
+    "speciesName": "Cinnamomum camphora",
+    "cf": 0.174,
+  },
+  {
+    "name": "牛樟",
+    "speciesName": "Cinnamomum micranthum",
+    "cf": 0.178,
+  },
+  {
+    "name": "光蠟樹",
+    "speciesName": "Fraxinus formosana",
+    "cf": 0.342,
+  },
+  {
+    "name": "大葉楠",
+    "speciesName": "Machilus kusanoi",
+    "cf": 0.218,
+  },
+  {
+    "name": "香楠",
+    "speciesName": "Machilus zuihoensis",
+    "cf": 0.221,
+  },
+  {
+    "name": "楝樹",
+    "speciesName": "Melia azedarach",
+    "cf": 0.252,
+  },
+  {
+    "name": "烏心石",
+    "speciesName": "Michelia compressa",
+    "cf": 0.247,
+  },
+  {
+    "name": "印度紫檀",
+    "speciesName": "Pterocarpus indicus",
+    "cf": 0.273,
+  },
+  {
+    "name": "木荷",
+    "speciesName": "Schima superba",
+    "cf": 0.286,
+  },
+  {
+    "name": "大葉桃花心木",
+    "speciesName": "Swietenia macrophylla",
+    "cf": 0.236,
+  },
+  {
+    "name": "台灣櫸",
+    "speciesName": "Zelkova serrata",
+    "cf": 0.348,
+  },
+]
