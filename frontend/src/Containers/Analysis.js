973ca5127ff73be4a259d332d5cb664ff2a5748f
Derek Lu <derekdylu@gmail.com>
2023-05-24 00:34:43 +0800

    feat: try add adsense

--- frontend/src/Containers/Analysis.js
+++ frontend/src/Containers/Analysis.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-import Navigation from '../Components/Navigation/Navigation'
+import Navigation from '../Components/Navigation/Navigation';
+import Footer from '../Components/Footer/Footer';
+import AdSense from 'react-adsense';
 
 const Analysis = () => {
   return (
     <div>
       <Navigation />
+      <AdSense.Google
+        client={process.env.AD_CLIENT_ID}
+        slot={process.env.AD_CLIENT_SLOT}
+      />
+      <Footer />
     </div>
   )
 }
