907d847f6469d75f43d081ade25cb7b375d54929
Derek Lu <derekdylu@gmail.com>
2023-05-26 13:14:11 +0800

    feat: try add analysis

--- frontend/src/Containers/Analysis.js
+++ frontend/src/Containers/Analysis.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 import Navigation from '../Components/Navigation/Navigation';
 import Footer from '../Components/Footer/Footer';
-import {Adsense} from '@ctrl/react-adsense';
+// import {Adsense} from '@ctrl/react-adsense';
+
+import Dashboard from '../Components/Dashboard/Dashboard';
 
 const Analysis = () => {
   return (
     <div>
       <Navigation />
-      <Adsense
-        client="ca-pub-9248017741391316"
-        slot="5990662559"
-      />
+        {/* <Adsense
+          client="ca-pub-9248017741391316"
+          slot="5990662559"
+        /> */}
+        <Dashboard />
       <Footer />
     </div>
   )
